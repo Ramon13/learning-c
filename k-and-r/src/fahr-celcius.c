abef412320b63eb09e203595456d85d05a7ce4ea
Ramon <ramon.santoscosta@yahoo.com>
2024-01-17 22:07:57 -0300

    add identation and decimal point for celcius temp

--- k-and-r/src/fahr-celcius.c
+++ k-and-r/src/fahr-celcius.c
@@ -3,15 +3,15 @@
 /*Example of book - ch1*/
 
 int main() {
-	int fahr = 0;
 	int lower = 0;
 	int upper = 300;
 	int step = 20;
-	int celcius;
+	float fahr = 0;
+	float celcius;
 
 	while (fahr <= upper) {
-		celcius = 5/9 * (fahr - 32);
-		printf("%d\t%d\n", fahr, celcius);
+		celcius = (5.0 / 9.0) * (fahr - 32.0);
+		printf("%3.0f %6.1f\n", fahr, celcius);
 		fahr += step;
 	}
 	
